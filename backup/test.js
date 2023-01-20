bce7eaf49211237f9722c6e14230b561eceb235b
yoneekee <yoneeki1177@gmail.com>
2023-01-20 15:35:24 +0900

    end

---
+++ backup/test.js
@@ -0,0 +1,7 @@
+// console.log("Math.random() * 10 : " + Math.random() * 10);
+
+console.log("parseInt(Math.random() * 3) : " + parseInt(Math.random() * 3)); // 0 ~ 2
+console.log("parseInt(Math.random() * 4) : " + parseInt(Math.random() * 4)); // 0 ~ 3
+console.log(
+  "parseInt(Math.random() * 10) * 10 : " + parseInt(Math.random() * 10) // 0 ~ 9
+);
--- js/main.js
+++ js/main.js
@@ -1,13 +1,105 @@
 const comItems = document.querySelectorAll("#com ul li");
 const playerItems = document.querySelectorAll("#player ul li");
+const resultList = document.querySelector("#result ul");
+const blocking = document.querySelector(".blocking");
+const cover = document.querySelector("#cover");
+const btnRestart = document.querySelector("#btnRestart");
+let num = 0; // 전역 변수 scope  let, const {}  var function() {}
+
+function appendWinItem() {
+  const li = document.createElement("li");
+  li.classList.add("win");
+  li.textContent = "W";
+  resultList.appendChild(li);
+}
+function appendDrawItem() {
+  const li = document.createElement("li");
+  li.classList.add("draw");
+  li.textContent = "D";
+  resultList.appendChild(li);
+}
+function appendLoseItem() {
+  const li = document.createElement("li");
+  li.classList.add("lose");
+  li.textContent = "L";
+  resultList.appendChild(li);
+}
+function appendItem(classTxt, txt) {
+  const li = document.createElement("li");
+  li.classList.add(classTxt);
+  li.textContent = txt;
+  resultList.appendChild(li);
+}
 
 function random() {
   for (let i = 0; i < 3; i++) {
     comItems[i].style.display = "none";
   }
-
-  const num = parseInt(Math.random() * comItems.length);
+  num = parseInt(Math.random() * comItems.length);
   comItems[num].style.display = "block";
 }
+let count = 0;
+let winCount = 0;
+let drawCount = 0;
+let loseCount = 0;
+for (let i = 0; i < playerItems.length; i++) {
+  playerItems[i].addEventListener("click", function () {
+    blocking.classList.add("on");
+    count++;
+    clearInterval(idx);
+    let idx02 = setTimeout(restart, 1000);
 
-setInterval(random, 20);
+    if (i === num) {
+      //비겼을때
+      appendItem("draw", "D");
+      drawCount++;
+    } else if ((i === 0 && num === 2) || (i === 1 && num === 0) || (i === 2 && num === 1)) {
+      //이긴경우
+      appendItem("win", "W");
+      winCount++;
+    } else {
+      //졌을경우
+      appendItem("lose", "L");
+      loseCount++;
+    }
+    if (count >= 3) {
+      clearTimeout(idx02);
+      //www, wwl,wwd,wdd, 이기는 case
+      //LLL,LLW,LLD,LDD  지는 경우
+      //DDD,WDL 비기는 경우
+      const msg = cover.querySelector(".msg");
+      if (winCount >= 2 || (drawCount === 2 && winCount === 1)) {
+        msg.textContent = "YOU WIN!!!";
+      } else if (loseCount >= 2 || (drawCount === 2 && loseCount === 1)) {
+        console.log("완전 짐");
+        msg.textContent = "YOU LOSE!!!";
+      } else {
+        console.log("완전 비김");
+        msg.textContent = "YOU DRAW!!!";
+      }
+      cover.classList.add("on");
+    }
+  });
+}
+btnRestart.addEventListener("click", function () {
+  cover.classList.remove("on");
+  count = 0;
+  winCount = 0;
+  loseCount = 0;
+  drawCount = 0;
+  const items = resultList.querySelectorAll("li");
+  for (let i = 0; i < 3; i++) {
+    items[i].remove();
+  }
+  // items[0].remove();
+  // items[1].remove();
+  // items[2].remove();
+  //resultList.innerHTML = "";
+  restart();
+});
+
+let idx = setInterval(random, 20);
+function restart() {
+  blocking.classList.remove("on");
+  idx = setInterval(random, 20);
+}
